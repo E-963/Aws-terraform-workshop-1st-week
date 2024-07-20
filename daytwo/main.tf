2564a425cb8929dfb8348b34623473cf763df162
E-963 <enanysama10@gmail.com>
2024-07-21 01:21:55 +0300

    Upadate Daytwo

--- daytwo/main.tf
+++ daytwo/main.tf
@@ -17,7 +17,16 @@ resource "aws_s3_bucket" "s3-1" {
   }
 }
 
-# Enable S3 Bucket Versioning.
+# Enable ACL as private
+resource "aws_s3_bucket_acl" "acl-s3-1" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership_s3-1]
+
+  bucket = aws_s3_bucket.s3-1.id
+  acl    = "private"
+
+
+}
+# Enable S3 Bucket Versioning
 resource "aws_s3_bucket_versioning" "version-1" {
   bucket = aws_s3_bucket.s3-1.id
   versioning_configuration {
@@ -26,15 +35,19 @@ resource "aws_s3_bucket_versioning" "version-1" {
 }
 
 # Create a directory in the S3 bucket
-resource "aws_s3_object" "directory" {
-  bucket = aws_s3_bucket.s3-1.bucket
-  key    = "logs/"
+resource "aws_s3_directory_bucket" "example" {
+  bucket = "main-s3-1--use1-az4--x-s3"
+
+  location {
+    name = "use1-az4"
+  }
 }
 
 # Get IAM user
 resource "aws_iam_user" "user" {
   name = "test-user" 
 }
+
 
 # Create policy document for the IAM user to allow upload only under logs/
 data "aws_iam_policy_document" "upload_object_user" {
@@ -46,4 +59,10 @@ data "aws_iam_policy_document" "upload_object_user" {
       identifiers = ["data.aws_iam_user.user.arn"]
     }
   }
+}
+# Create S3 Object (i.e directories)
+resource "aws_s3_object" "directory_object_s3-1_public" {
+  bucket = aws_s3_bucket.s3-1.id
+  key = "Public/"
+  content_type = "application/x-directory"
 }
