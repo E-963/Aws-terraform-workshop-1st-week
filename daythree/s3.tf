81918c4b6fd844e02e878f4d79a846a5b5ee7d32
E-963 <email@address.com>
2024-07-22 09:56:47 +0300

    Worshops Update

---
+++ daythree/s3.tf
@@ -0,0 +1,122 @@
+resource "aws_s3_bucket" "buc-1" {
+  bucket = "main-buc-1"
+
+  force_destroy       = true   # force destroy even if the bucket not empty
+  object_lock_enabled = false
+
+  tags = {
+    Name        = "erakiterrafromstatefiles"
+    Environment = "terraformChamps"
+    Owner       = "Sama"
+  }
+}
+# Create outgoing directory
+resource "aws_s3_object" "directory_object_buc-1_outgoing" {
+  bucket = aws_s3_bucket.buc-1.id
+  key = "outgoing/"
+  content_type = "application/x-directory"
+}
+
+# Create incoming directory
+resource "aws_s3_object" "directory_object_buc-1_incoming" {
+  bucket = aws_s3_bucket.buc-1.id
+  key = "incoming/"
+  content_type = "application/x-directory"
+}
+
+# Create logs directory
+resource "aws_s3_object" "directory_object_buc-1_logs" {
+  bucket = aws_s3_bucket.buc-1.id
+  key = "logs/"
+  content_type = "application/x-directory"
+}
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-lifecycle-config" {
+  bucket = aws_s3_bucket.buc-1.id
+
+ # logs directory rule
+  rule {
+    id = "log-directory"
+
+    filter {  # filter the bucket based on the path prefix 
+        prefix = "logs/"
+    }
+
+    transition {  # move the files to infrequent access tier 30 days after creation time
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    status = "Enabled"
+
+        transition {
+     days = 90
+      storage_class = "GLACIER"
+    }
+    
+     transition {  # move the files to the deep archive tier 180 days after creation time.
+      days = 180
+      storage_class = "DEEP_ARCHIVE"
+    }
+
+    expiration { # Delete objects a year after creation time.
+      days = 365
+    }
+  }
+  
+
+  # outgoing directory rule
+  rule {
+    id = "outgoing"
+       filter {
+      tag {          # filter objects based on objects tag & under outgoing directory object
+        key = "Name"
+        value = "notDeepArchvie"
+      }
+      and {
+        prefix = "outgoing/"
+      }
+    }
+    transition {     # Move to Infrequent access tier 30 days after creation time
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {  # Move to Archive access tier 90 days after creation time
+      days = 90
+      storage_class = "GLACIER"
+    }
+    expiration {  # Delete objects a year after creation time.
+      days = 365
+    }
+
+    # Enable the rule
+    status = "Enabled"
+  }
+
+
+ # incoming directory rule 
+  rule {
+    id = "incoming-directory"
+
+    filter {
+        prefix = "incoming/"
+
+        # Filter files it's size is between 1MB to 1G
+        object_size_greater_than = 1000000 # in Bytes
+        object_size_less_than = 100000000  # in Bytes
+    }
+
+    transition {          # Move to Infrequent access tier 30 days after creation time
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+  
+    transition {          # Move to Archive access tier 90 days after creation time
+      days = 90
+      storage_class = "GLACIER"
+    }
+
+    # Enable the rule
+    status = "Enabled"
+  }
+}
--- daytwo/main.tf
+++ daytwo/main.tf
@@ -11,14 +11,9 @@ resource "aws_s3_bucket" "s3-1" {
     Owner       = "Sama"
   }
 }
-# create ACL
-resource "aws_s3_bucket_acl" "acl-1" {
-  bucket = aws_s3_bucket.s3-1.id
-  acl    = "private"
-}
 
 # Enable S3 Bucket Versioning
-resource "aws_s3_bucket_versioning" "version-1" {
+resource "aws_s3_bucket_versioning" "ver_01" {
   bucket = aws_s3_bucket.s3-1.id
   versioning_configuration {
     status = "Enabled"
@@ -26,25 +21,38 @@ resource "aws_s3_bucket_versioning" "version-1" {
 }
 
 # Get IAM user
-resource "aws_iam_user" "user" {
+resource "aws_iam_user" "Iam-1" {
   name = "test-user" 
 }
 
+# Create bucket policy by mentioning the policy below (assuming upload_object_to_logs.json is configured correctly)
+resource "aws_s3_bucket_policy" "Policy-s3-1"  {
+  bucket = aws_s3_bucket.s3-1.id
+  policy = data.aws_iam_policy_document.upload_object_to_logs.json
+}
 
 # Create policy document for the IAM user to allow upload only under logs/
-data "aws_iam_policy_document" "upload_object_user" {
+data "aws_iam_policy_document" "upload_object_to_logs"{
   statement {
-    actions = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.s3-1.arn}/logs/*"]
     principals {
       type        = "AWS"
-      identifiers = ["data.aws_iam_user.user.arn"]
+       identifiers = [aws_iam_user.Iam-1.arn]  # Use the actual user ARN
     }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.s3-1.arn,
+      "${aws_s3_bucket.s3-1.arn}/logs/*",
+    ]
   }
 }
 
 # Create S3 Object (i.e directories)
-resource "aws_s3_object" "directory_object_s3-1_public" {
+resource "aws_s3_object" "dir-1" {
   bucket = aws_s3_bucket.s3-1.id
   key = "logs/"
   content_type = "application/x-directory"
